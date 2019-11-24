c8c64a9248d1aff223ab0dcccaabd46288e41f78
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-24 11:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
 
-return Object.assign({{}, object, {[key]:value}})
+return Object.assign({}, object, {[key]:value})
 
 }
