a30f153e9f68412241192a866ae319b13b977078
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-24 11:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, object, {[key]:value})
 
 }
+
+destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+object[key] = value
+
+return object
+
+}
